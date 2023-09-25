6eff794b57b033fee8076d9173267eaf3a108da7
SalmanTCM <salmann.hossain@gmail.com>
2023-09-25 22:53:16 +0600

    Ticket Management

--- ticket_management/admin.py
+++ ticket_management/admin.py
@@ -1,16 +1,7 @@
 # ticket_management/admin.py
 
 from django.contrib import admin
-from .models import Customer, salesLog
+from .models import Ticket, Agent
 
-@admin.register(Customer)
-class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('pax_first_name', 'pax_last_name', 'passport_no', 'pnr_no', 'email', 'phone_number', 'travel_date', 'address')
-    list_filter = ('travel_date',)
-    search_fields = ('pax_first_name', 'pax_last_name', 'passport_no', 'pnr_no', 'email')
-
-@admin.register(salesLog)
-class SalesLogAdmin(admin.ModelAdmin):
-    list_display = ('customer', 'route', 'ticket_no', 'total', 'received', 'status', 'remains', 'sale_date')
-    list_filter = ('sale_date',)
-    search_fields = ('customer__pax_first_name', 'customer__pax_last_name', 'route', 'ticket_no', 'status')
+admin.site.register(Ticket)
+admin.site.register(Agent)
--- ticket_management/models.py
+++ ticket_management/models.py
@@ -1,28 +1,44 @@
-# ticket_management/models.py
-
 from django.db import models
+from Customer.models import Customer
 
-class Customer(models.Model):
-    pax_first_name = models.CharField(max_length=100, null=True)
-    pax_last_name = models.CharField(max_length=100, null=True)
-    passport_no = models.CharField(max_length=100, null=True)
-    pnr_no = models.CharField(max_length=100, null=True)
-    email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
-    travel_date = models.DateField()
-    address = models.TextField(blank=True, null=True)
 
-class salesLog(models.Model):
+class Ticket(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    route = models.CharField(max_length=100, null=True)
-    ticket_no = models.CharField(max_length=100, null=True)
-    total = models.CharField(max_length=100)
-    received = models.CharField(max_length=100)
-    status = models.CharField(max_length=100)
-    remains = models.CharField(max_length=100)
-    sale_date = models.DateField()
-    # Add more fields as needed
+    agent = models.ForeignKey('Agent', on_delete=models.CASCADE)
+    from_location = models.CharField(max_length=255)
+    to_location = models.CharField(max_length=255)
+    departure_date = models.DateField()
+    ticket_class = models.CharField(max_length=50)
+    quantity = models.IntegerField()
+    note = models.TextField(blank=True, null=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    received = models.BooleanField(default=False)
+    payment = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    paid = models.BooleanField(default=False)
+    terminated = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Ticket #{self.id} - {self.customer.first_name} {self.customer.last_name}"
 
 
-class ticket_management (models.Model):
-    status = models.CharField(max_length=100, null=True)
+class Agent(models.Model):
+    first_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField(unique=True, blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    payment_method = models.CharField(
+        max_length=10,
+        choices=[('bank', 'Bank'), ('mfs', 'Mobile Financial Service'), ('cash', 'Cash')],
+        default='bank'
+    )
+    remarks = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return f"Agent #{self.id}"
+
+
