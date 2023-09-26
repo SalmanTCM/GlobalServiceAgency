fd6fe78a2b0031f1f1fbc505e11c50ea88c6d0a7
SalmanTCM <salmann.hossain@gmail.com>
2023-09-26 23:11:14 +0600

    list of ticket and agent

--- ticket_management/admin.py
+++ ticket_management/admin.py
@@ -3,5 +3,17 @@
 from django.contrib import admin
 from .models import Ticket, Agent
 
-admin.site.register(Ticket)
-admin.site.register(Agent)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'agent', 'from_location', 'to_location', 'departure_date', 'ticket_class', 'quantity', 'note', 'amount', 'received', 'payment', 'paid', 'terminated', 'created_at', 'updated_at')
+    list_filter = ('customer', 'from_location')
+    search_fields = ('ticket_class', 'agent', 'customer', 'terminated')
+
+admin.site.register(Ticket, TicketAdmin)
+
+
+class AgentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'phone_number', 'payment_method', 'remarks')
+    list_filter = ('payment_method',)
+    search_fields = ('phone_number', 'email')
+
+admin.site.register(Agent, AgentAdmin)
